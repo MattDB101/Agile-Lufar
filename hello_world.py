e530ef53477d351e512186074f9761ffc7334bff
Matthew Byrne <matt@invi.so>
2021-12-08 22:30:59 +0000

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 18 10:38:41 2021
+
+@author: dylan
+"""
+
 print('Hello World!')
+
+x = 4
+y = 2
+
+result = x + y
+print(result)
